5faf78c0a0d418fb438b85d306fb7db0ec507d80
aditya <latitik.aditya@gmail.com>
2022-05-07 08:13:05 +0530

    restfull services

--- src/main/java/com/zorba/aig/life/insurance/controller/EmployeeController.java
+++ src/main/java/com/zorba/aig/life/insurance/controller/EmployeeController.java
@@ -5,31 +5,35 @@ import com.zorba.aig.life.insurance.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/api/employees")
+    @GetMapping("/employees")
     public List<Employee> allEmp(){
         return employeeService.getAllEmployee();
     }
 
-    @PostMapping(value = "/api/employee",consumes = "application/json",produces = "application/json")
-    public Employee addEmployee(@RequestBody Employee employee){
+    @PostMapping(value = "/employee",consumes = "application/json",produces = "application/json")
+    public Employee addEmployee(@RequestBody @Valid Employee employee){
         return employeeService.saveEmployee(employee);
     }
 
 
-    @PutMapping(value = "/api/employee",consumes = "application/json",produces = "application/json")
+    @PutMapping(value = "/employee",consumes = "application/json",produces = "application/json")
     public String update(@RequestBody Employee employee){
-        String status="";
+        String status=null;
 
+        status.length();
         try {
             employeeService.saveEmployee(employee);
+
             status="sucess";
         }catch (Exception e){
             status="failure";
@@ -38,12 +42,12 @@ public class EmployeeController {
         return status;
     }
 
-    @GetMapping(value = "/api/employee/{id}")
+    @GetMapping(value = "/employee/{id}")
     public Employee findEmployeeById(@PathVariable("id") int id){
         return employeeService.findEmployeeById(id);
     }
 
-    @DeleteMapping(value = "/api/employee")
+    @DeleteMapping(value = "/employee")
     public String deleteEmpById(@RequestParam("id") int id){
 
         String status ="";
